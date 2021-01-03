adcbed042fc1e84debcabbd525bb780041acce29
zainu-99 <zainu.developer@gmail.com>
2021-01-03 19:10:54 +0700

    developement

--- src/main/java/com/myapp/spring/SecSecurityConfig.java
+++ src/main/java/com/myapp/spring/SecSecurityConfig.java
@@ -6,8 +6,10 @@
 package com.myapp.spring;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -16,27 +18,30 @@ import org.springframework.security.crypto.password.PasswordEncoder;
  *
  * @author zainu
  */
+@Configuration
+@EnableWebSecurity
 public class SecSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
-        .withUser("user1").password(passwordEncoder().encode("user1Pass")).roles("USER")
-        .and()
-        .withUser("user2").password(passwordEncoder().encode("user2Pass")).roles("USER")
-        .and()
-        .withUser("admin").password(passwordEncoder().encode("adminPass")).roles("ADMIN");
+			.withUser("admin")
+			.password("admin")
+			.authorities("ADMIN");
     }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
         .csrf().disable()
         .authorizeRequests()
-        .antMatchers("/admin/**").hasRole("ADMIN")
+        .antMatchers("/**").hasRole("ADMIN")
         .antMatchers("/anonymous*").anonymous()
         .antMatchers("/login*").permitAll()
         .anyRequest().authenticated()
-        .and();
+        .and()
+        .logout()
+        .logoutUrl("/perform_logout")
+        .deleteCookies("JSESSIONID");
     }
     
     @Bean 
